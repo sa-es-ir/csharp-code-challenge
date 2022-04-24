7586957c4ee94787a77e885ac16e13493443a2d0
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-04-24 23:47:38 +0430

    century from year challenge test

---
+++ test/CodeChallenge.Test/CenturyFromYearChallengeTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeChallenge.Challenges;
+using FluentAssertions;
+using Xunit;
+
+namespace CodeChallenge.Test;
+
+public class CenturyFromYearChallengeTests
+{
+
+    [Theory]
+    [InlineData(1905, 20)]
+    [InlineData(2009, 21)]
+    public void CenturyFromYear_ValidYear_ReturnResult(int year, int expected)
+    {
+        //arrange
+        var challenge = new CenturyFromYearChallenge();
+
+        //act
+        var result = challenge.Do(year);
+
+        //assert
+        result.Should().Be(expected);
+    }
+}
