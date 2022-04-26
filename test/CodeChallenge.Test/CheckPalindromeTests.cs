4d6e42389d2af1a47a243cc913eedc50a7d4ec47
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-04-26 23:26:36 +0430

    add check palindrome challenge tests

---
+++ test/CodeChallenge.Test/CheckPalindromeTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using CodeChallenge.Challenges;
+using FluentAssertions;
+using Xunit;
+
+namespace CodeChallenge.Test;
+
+public class CheckPalindromeTests
+{
+
+    [Theory]
+    [InlineData("aabaa", true)]
+    [InlineData("a", true)]
+    [InlineData("abac", false)]
+    [InlineData("cccc", true)]
+    public void CheckPalindrome_ValidInput_ReturnResult(string input, bool expected)
+    {
+        //arrange
+        var challenge = new CheckPalindrome();
+
+        //act
+        var result = challenge.Do(input);
+
+        //assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void CheckPalindrome_InvalidEmptyInput_ReturnException()
+    {
+        //arrange
+        var challenge = new CheckPalindrome();
+        var input = string.Empty;
+
+        //act
+        Action result = () => challenge.Do(input);
+
+        //assert
+        result.Should().Throw<ArgumentException>().WithMessage("input is empty");
+    }
+
+    [Fact]
+    public void CheckPalindrome_InvalidLowerCaseInput_ReturnException()
+    {
+        //arrange
+        var challenge = new CheckPalindrome();
+        var input = "AvbRd";
+
+        //act
+        Action result = () => challenge.Do(input);
+
+        //assert
+        result.Should().Throw<ArgumentException>().WithMessage("input should be lowercase");
+    }
+}
