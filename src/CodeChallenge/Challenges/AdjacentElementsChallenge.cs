1cb2e3abcb1aebba16dd6c9a94cc7397b7c60fd1
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-04-27 22:54:07 +0430

    add requirement adjacent elements challenge

--- src/CodeChallenge/Challenges/AdjacentElementsChallenge.cs
+++ src/CodeChallenge/Challenges/AdjacentElementsChallenge.cs
@@ -7,6 +7,9 @@ public class AdjacentElementsChallenge
         if (inputArray.Length is < 2 or > 10)
             throw new ArgumentOutOfRangeException(nameof(inputArray), "invalid array length");
 
+        if (inputArray.Any(x => x is < -1000 or > 1000))
+            throw new Exception("invalid array item");
+
         return inputArray.Select((i, j) => j > 0 ? i * inputArray[j - 1] : int.MinValue).Max();
     }
 }
