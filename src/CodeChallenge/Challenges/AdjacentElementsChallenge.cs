b51a130388ce22886f0e895ae08fce347a24c11d
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-04-27 22:47:05 +0430

    add adjacent elements challenge

---
+++ src/CodeChallenge/Challenges/AdjacentElementsChallenge.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeChallenge.Challenges;
+
+public class AdjacentElementsChallenge
+{
+    public int Do(int[] inputArray)
+    {
+        if (inputArray.Length is < 2 or > 10)
+            throw new ArgumentOutOfRangeException(nameof(inputArray), "invalid array length");
+
+        int maxValue = int.MinValue;
+        for (int i = 0; i < inputArray.Length - 1; i++)
+        {
+            var j = i + 1;
+            var value = inputArray[i] * inputArray[j];
+            if (value > maxValue)
+                maxValue = value;
+        }
+
+        return maxValue;
+    }
+}
