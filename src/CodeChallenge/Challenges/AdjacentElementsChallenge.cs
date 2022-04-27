5b619acc3baa6167e4c861802e5cd6381296c4ee
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-04-27 22:50:15 +0430

    refactor adjacent elements challenge

--- src/CodeChallenge/Challenges/AdjacentElementsChallenge.cs
+++ src/CodeChallenge/Challenges/AdjacentElementsChallenge.cs
@@ -7,15 +7,6 @@ public class AdjacentElementsChallenge
         if (inputArray.Length is < 2 or > 10)
             throw new ArgumentOutOfRangeException(nameof(inputArray), "invalid array length");
 
-        int maxValue = int.MinValue;
-        for (int i = 0; i < inputArray.Length - 1; i++)
-        {
-            var j = i + 1;
-            var value = inputArray[i] * inputArray[j];
-            if (value > maxValue)
-                maxValue = value;
-        }
-
-        return maxValue;
+        return inputArray.Select((i, j) => j > 0 ? i * inputArray[j - 1] : int.MinValue).Max();
     }
 }
