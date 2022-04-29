147be1700e02c45df4a61b98271a69a906a83689
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-04-29 17:51:19 +0430

    add shape area challenge test

---
+++ test/CodeChallenge.Test/ShapeAreaChallengeTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeChallenge.Challenges;
+using FluentAssertions;
+using Xunit;
+
+namespace CodeChallenge.Test;
+
+public class ShapeAreaChallengeTests
+{
+    [Theory]
+    [InlineData(2, 5)]
+    [InlineData(3, 13)]
+    public void ShapeArea_ValidInput_ReturnResult(int n, int expected)
+    {
+        //arrange
+        var challenge = new ShapeAreaChallenge();
+
+        //act
+        var result = challenge.Do(n);
+
+        //assert
+        result.Should().Be(expected);
+    }
+}
