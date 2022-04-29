51c0ae5ad3a86373f34bc3ea24d85c46a958f409
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-04-29 17:48:04 +0430

    add shape area challenge

---
+++ src/CodeChallenge/Challenges/ShapeAreaChallenge.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeChallenge.Challenges;
+
+public class ShapeAreaChallenge
+{
+    public int Do(int n)
+    {
+        if (n == 1)
+            return 1;
+
+        var center = n + (n - 1);
+        var counter = center;
+
+        while (counter > 1)
+        {
+            counter -= 2;
+            center += 2 * counter;
+        }
+
+        return center;
+    }
+}
