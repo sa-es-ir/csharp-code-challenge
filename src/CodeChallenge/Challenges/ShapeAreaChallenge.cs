1f5d094924d6e5795c522224f7de053f3ab4f8f0
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-04-30 23:50:18 +0430

    fix error messag

--- src/CodeChallenge/Challenges/ShapeAreaChallenge.cs
+++ src/CodeChallenge/Challenges/ShapeAreaChallenge.cs
@@ -5,7 +5,7 @@ public class ShapeAreaChallenge
     public int Do(int n)
     {
         if (n is < 1 or > 10000)
-            throw new Exception($"int should be between 1 to 10000 but current is {n}");
+            throw new Exception($"input should be between 1 to 10000 but current is {n}");
 
         if (n == 1)
             return 1;
