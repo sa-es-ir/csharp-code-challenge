fdd57fd019010523f5340e9d81421bfe72e8ad15
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-05-03 23:35:20 +0430

    add make array consecutive 2 challenge test

---
+++ test/CodeChallenge.Test/MakeArrayConsecutive2ChallengeTests.cs
@@ -0,0 +1,24 @@
+﻿using CodeChallenge.Challenges;
+using FluentAssertions;
+using Xunit;
+
+namespace CodeChallenge.Test;
+
+public class MakeArrayConsecutive2ChallengeTests
+{
+    [Theory]
+    [InlineData(new[] { 6, 2, 3, 8 }, 3)]
+    [InlineData(new[] { 5, 2, 3, 1 }, 1)]
+    [InlineData(new[] { 5, 2, 3, 10 }, 5)]
+    public void MakeArrayConsecutive_InvalidInput_ReturnsResult(int[] items, int expected)
+    {
+        //arrange
+        var challenge = new MakeArrayConsecutive2Challenge();
+
+        //act
+        var result = challenge.Do(items);
+
+        //asset
+        result.Should().Be(expected);
+    }
+}
