792d74486fd9a068277d30a5a9b264cda66a42e3
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-05-08 23:22:52 +0430

    add longest common prefix

---
+++ src/CodeChallenge/Challenges/LongestCommonPrefixChallenge.cs
@@ -0,0 +1,35 @@
+﻿namespace CodeChallenge.Challenges;
+
+public class LongestCommonPrefixChallenge
+{
+    public string Do(List<string> array)
+    {
+        if (array == null) throw new ArgumentNullException(nameof(array));
+
+        array = array.OrderBy(_ => _).ToList();
+
+        int longestCount = 1;
+        string longestPrefix = string.Empty;
+        for (int i = 1; i <= array.Max(_ => _)?.Length; i++)
+        {
+            var grouped = array.Where(x => x.Length >= i)
+                .GroupBy(x => x[..i])
+                .Where(x => x.Count() > 1)
+                .OrderByDescending(x => x.Count())
+                .Select(x => new { LongestCommonPrefix = x.Key, LongestCommonPrefixCount = x.Count() })
+                .FirstOrDefault();
+
+            if (grouped == null)
+                break;
+
+            if (grouped.LongestCommonPrefix.Length > longestPrefix.Length)
+            {
+                longestCount = grouped.LongestCommonPrefixCount;
+                longestPrefix = grouped.LongestCommonPrefix;
+            }
+        }
+
+        return longestPrefix;
+
+    }
+}
--- src/CodeChallenge/Program.cs
+++ src/CodeChallenge/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using CodeChallenge.Challenges;
+
+Console.WriteLine("Hello, World!");
+
+var commonPrefix = new LongestCommonPrefixChallenge();
+var result = commonPrefix.Do(new List<string>() { "a", "a3bC", "a2bcD", "abcz" });
+
+Console.Read();
