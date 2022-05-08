78932c9ac0cf66a59f1357ae8a51b1d1548f36c0
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-05-08 23:51:42 +0430

    add longest common prefix tests

---
+++ test/CodeChallenge.Test/LongestCommonPrefixChallengeTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using CodeChallenge.Challenges;
+using FluentAssertions;
+using Xunit;
+
+namespace CodeChallenge.Test;
+
+public class LongestCommonPrefixChallengeTests
+{
+    [Theory]
+    [InlineData(new[] { "a", "abC", "abcD" }, "ab")]
+    [InlineData(new[] { "a", "abC", "abcD", "abcwr" }, "abc")]
+    [InlineData(new[] { "a", "bC", "g1bcD" }, "")]
+    public void LongestCommonPrefix_ValidInput_ReturnResult(string[] array, string expected)
+    {
+        //arrange
+        var challenge = new LongestCommonPrefixChallenge();
+
+        //act
+        var result = challenge.Do(array);
+
+        //assert
+        result.Should().Be(expected);
+    }
+}
